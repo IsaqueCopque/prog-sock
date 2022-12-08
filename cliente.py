c60e274a8935f4eba8554f0e66ae7d08c295a9de
Matheus Novais <matheusnovaisz@gmail.com>
2022-12-08 14:57:24 -0300

    append cliente.py

--- cliente.py
+++ cliente.py
@@ -1,5 +1,6 @@
 import socket
-from os.path import exists, getsize
+from os.path import exists, getsize, normpath
+from utils import check_file
 
 # Variáveis Globais
 HOST = socket.gethostbyname(socket.gethostname())
@@ -12,18 +13,6 @@ SEPARATOR = "<SEPARATOR>"
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # ---
 
-def check_file(arq):
-    """
-    Verifica se o arquivo informado pelo cliente é válido e existe.
-    """
-    dotIndex = arq.find('.')
-    if dotIndex != -1:
-        finalP = len(arq)-1
-        if finalP-dotIndex > 0: #possui extensao depois do '.'
-            return exists(arq)
-    else:
-        return False
-
 def get_input():
     """
     Obtém do cliente a operação a ser realizada e o nome de arquivo.
@@ -33,7 +22,7 @@ def get_input():
         op = input("Informe a operação a ser realizada: (D) Depósito (R) Recuperação")
         if op == 'D' or op == 'd' or op == 'R' or op == 'r':
             arq = input("Informe o arquivo a ser armazenado: ")
-            if not check_file(arq): #Arquivo inválido
+            if (op == 'D' or op == 'd') and not check_file(arq): #Arquivo inválido
                 print("Arquivo inválido")
                 continue
             fLevel = None
@@ -45,35 +34,45 @@ def connect_to_server():
     try:
         sock.connect((HOST,PORT))
         op, arq, fLevel = get_input()
-        arqSize = getsize(arq) #tamanho do arquivo a ser enviado
+        #arqSize = getsize(arq) #tamanho do arquivo a ser enviado
         if op == 'D' or op == 'd': #Depósito
             send_file_to_server(arq, fLevel)
             #send_to_server("Mensagem DEPOSITO") #POR ENQUANTO SÓ MANDA MENSAGEM
         else: #Recuperação
-            send_to_server("Mensagem RECUPERACAO") #POR ENQUANTO SÓ MANDA MENSAGEM
+            get_file_from_server(arq)
+            #send_to_server("Mensagem RECUPERACAO") #POR ENQUANTO SÓ MANDA MENSAGEM
     except Exception as e:
         print("-> Erro ao criar conexão com servidor.", e.__class__)
 
 def send_file_to_server(arq, fLevel):
     arqSize = getsize(arq) #tamanho do arquivo a ser enviado
-    encData = f"{arq}{SEPARATOR}{arqSize}{SEPARATOR}{fLevel}".encode('utf-8')
+    encData = f"d{SEPARATOR}{arq}{SEPARATOR}{arqSize}{SEPARATOR}{fLevel}".encode('utf-8')
     sock.send(encData)
     with open(arq, "rb") as f:
         while True:
             bytes_read = f.read(BUFFER_SIZE)
             if not bytes_read:
                 break
-            sock.send(bytes_read)
+            sock.sendall(bytes_read)
     res = sock.recv(1024).decode('utf-8')
     print(res)
 
-def send_to_server(data):
-    """
-    Envia dados ao servidor. POR ENQUANTO SÓ MANDA MENSAGEM
-    """
-    encData = data.encode('utf-8')
+def get_file_from_server(arq):
+    encData = f"r{SEPARATOR}{arq}{SEPARATOR}0{SEPARATOR}0".encode('utf-8')
     sock.send(encData)
-    #TESTA RESPOSTA
+    found, filesize = sock.recv(1024).decode('utf-8').split(SEPARATOR)
+    filesize = int(filesize)
+    filename = normpath(arq)
+    print(f"Arquivo com tamanho {filesize} bytes\n")
+    if bool(found):
+        data_received = 0
+        with open(filename, "wb") as f:
+            while filesize > data_received:
+                bytes_read = sock.recv(BUFFER_SIZE)
+                if not bytes_read:
+                    break
+                f.write(bytes_read)
+                data_received += BUFFER_SIZE
     res = sock.recv(1024).decode('utf-8')
     print(res)
 
